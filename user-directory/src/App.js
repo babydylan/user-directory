32fc17a63c0ad0b6155d6d7fa1eb0c0a0f3ec004
Dylan Nguyen <babydylannguyen@gmail.com>
2020-11-23 15:45:07 -0800

    added placehgolder table and API call

--- user-directory/src/App.js
+++ user-directory/src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
 import './App.css';
+import { Table } from "react-bootstrap";
+
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+  const styles = {
+    header: {
+      backgroundColor: "#6495ED",
+      padding: "1rem",
+      margin: "0"
+    },
+    input: {
+      marginLeft: "auto",
+      marginRight: "auto",
+      padding: "5px",
+    }
+
+  }; return (
+    <div>
+      <div className="App">
+        <header>
+          <h1 style={styles.header}>Search for Employee</h1>
+        </header>
+      </div>
+
+      <div>
+        <h2>
+          Search:
+        </h2>
+        <input style={styles.input}></input>
+      </div>
+
+      <Table striped bordered hover>
+        <thead>
+          <tr>
+            <th>#</th>
+            <th>First Name</th>
+            <th>Last Name</th>
+            <th>Username</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr>
+            <td>1</td>
+            <td>Mark</td>
+            <td>Otto</td>
+            <td>@mdo</td>
+          </tr>
+          <tr>
+            <td>2</td>
+            <td>Jacob</td>
+            <td>Thornton</td>
+            <td>@fat</td>
+          </tr>
+          <tr>
+            <td>3</td>
+            <td colSpan="2">Larry the Bird</td>
+            <td>@twitter</td>
+          </tr>
+        </tbody>
+      </Table>
     </div>
   );
 }
